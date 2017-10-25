37c3a22935cbdb5c89c6816e8b5113ba40e3c4d5
Justin Lemke <epicspyninja@gmail.com>
2017-10-25 00:01:49 -0700

    add windows powershell runscripts for java client/server

---
+++ client/run.ps1
@@ -0,0 +1,8 @@
+# powershell client start script.
+# expects you to have javac and java in your path.
+rm bin/*
+cd src
+javac -cp "..\lib\*;." Main.java -d ..\bin
+cd ..\bin
+java -cp "..\lib\*;." Main
+cd ..
